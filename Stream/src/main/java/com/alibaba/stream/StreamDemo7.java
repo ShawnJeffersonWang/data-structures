e75019484ce21ded13ef4cf38ba57cb7fd8c1a0d
shawn <xw2002121@gmail.com>
2024-01-25 17:57:28 +0800

    streamDemo

---
+++ Stream/src/main/java/com/alibaba/stream/StreamDemo7.java
@@ -0,0 +1,24 @@
+package com.alibaba.stream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class StreamDemo7 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list1 = new ArrayList<>();
+        Collections.addAll(list1, "a", "a", "b", "b", "b", "c", "d");
+
+        ArrayList<String> list2 = new ArrayList<>();
+        Collections.addAll(list2, "a", "a", "b", "b", "b", "c", "d");
+
+        list1.stream().distinct().forEach(s -> System.out.println(s));
+
+        System.out.println("===================");
+        Stream.concat(list1.stream(), list2.stream()).forEach(s -> System.out.println(s));
+
+
+    }
+}
